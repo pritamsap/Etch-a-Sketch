2bc0fdce5b271aec1cf3827cc57b9df9a72c5cce
PritamSapkota <slicefrost1@gmail.com>
2022-08-31 13:27:09 -0400

    Update newGridApply function
    	The updated newGridApply function calls reset() function which uses querySelectorAll to select all the grid boxes, find it's parents and remove all grids. Afterward, in the input element, user is able to enter a custom grid size and press 'New Grid' button - the newGridApply function takes the grid size and creates  a grid layout based on X by X user input.

--- js-file.js
+++ js-file.js
@@ -1,42 +1,57 @@
 
-const grid = document.createElement("div");
 const container = document.querySelector('.container');
-const hoverEffect = document.querySelector('.each-grid:hover')
+const gridBox = document.createElement('div');
+gridBox.classList.add('grid-box');
+const sideBar = document.querySelector('.side-bar');
+const newGridBtn = document.querySelector('.new-grid');
+const gridInput = document.querySelector('.grid-input');
 
 
-for(let i = 0; i < 256; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add('each-grid');
-    container.appendChild(grid);
+//creates a 16 x 16 grid
+for(let i  = 0; i < 256; i++) {
+    const gridBox = document.createElement('div');
+    gridBox.classList.add('grid-box');
+    container.appendChild(gridBox);
 }
 
 
-const grids = document.querySelectorAll('.each-grid');
-
-grids.forEach((eachGrid) => {
-
-    eachGrid.addEventListener('mousedown', () => {
-        eachGrid.setAttribute('style', 'background-color: black;');
-
-    });
-
-
-    
-    
-});
+newGridBtn.addEventListener('click', newGridApply);
 
 
 
+function newGridApply() {
+    reset();
+    var value = gridInput.value;
+    value = Number(value.substring(0, 2));
+    valueMultiply = idealGridValue(value);
 
+    for(let i = 0; i < valueMultiply; i++) {
+        const gridBox = document.createElement('div');
+        gridBox.classList.add('grid-box');
+        container.appendChild(gridBox);
+    }
 
+    container.setAttribute('style', `display: grid;  grid-template-columns: repeat(${value}, auto); grid-template-rows: repeat(${value}, auto)`);
 
-
-
+}
 
 
 
 
+// fixes any spaces or upper case issues written by the user
+function idealGridValue(value) {
+    const valueMultiply  = value * value;
+    return valueMultiply;
+}
 
+
+
+
+
+function reset() {
+    document.querySelectorAll(".grid-box")
+    .forEach((e) => e.parentNode.removeChild(e));
+}
 
 
 
